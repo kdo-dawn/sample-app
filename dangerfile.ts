d289e2aa626032dde981d926003c92fe9db824f8
Kristiyan Dobrev <117360933+kdo-dawn@users.noreply.github.com>
2022-11-09 14:35:13 +0100

    Installed DangerJS

---
+++ dangerfile.ts
@@ -0,0 +1,10 @@
+import { danger, fail, markdown, warn } from "danger"
+
+// Setup
+const pr = danger.github.pr
+
+// Always ensure we assign someone, so that our Slackbot can do its work correctly
+if (pr.assignee === null) {
+    fail("Please assign someone to merge this PR, and optionally include people who should review.")
+ }
+  
