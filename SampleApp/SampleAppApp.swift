3c411b848de97a17f407f20658503d2f7908a0ab
Kristiyan Dobrev <117360933+kdo-dawn@users.noreply.github.com>
2022-11-09 14:25:34 +0100

    Initial Commit

---
+++ SampleApp/SampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleAppApp.swift
+//  SampleApp
+//
+//  Created by Kristiyan Dobrev on 09/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct SampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
