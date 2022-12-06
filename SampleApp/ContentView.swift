03230e6c0c0ff44159dabcdafcf843db096afaf1
Kristiyan Dobrev <kdo@dawnhealth.com>
2022-12-06 13:14:40 +0100

    test

--- SampleApp/ContentView.swift
+++ SampleApp/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, danger!!")
+            Text("Hello, danger!!!")
         }
         .padding()
     }
